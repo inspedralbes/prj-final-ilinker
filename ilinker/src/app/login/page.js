4d58e2275d040e1fc8d8295d1a792e655fa2228a
Purvish Patel <a23purpatmah@inspedralbes.cat>
2025-01-27 09:18:51 +0100

    login

--- ilinker/src/app/login/page.js
+++ ilinker/src/app/login/page.js
@@ -1,60 +1,100 @@
+"use client";
 import Image from "next/image";
-export default async function Login() {
-    await new Promise((resolve) => setTimeout(resolve, 3000)); // Simula un retraso
-    return (
-        <div className="flex min-h-screen items-center justify-center bg-gray-100">
-            <div className="w-full max-w-md p-8 space-y-8 bg-white shadow-lg rounded-lg">
-                <Image className='mx-auto' src='/images/logo_nombre.svg' alt='logo' width={150} height={58}/>
+import { useState } from "react";
 
-                <form className="space-y-6">
-                    {/* Campo de correo electrónico */}
-                    <div>
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-600">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            required
-                            className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-                            placeholder="Enter your email"
-                        />
-                    </div>
+export default  function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-                    {/* Campo de contraseña */}
-                    <div>
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-600">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            required
-                            className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-                            placeholder="Enter your password"
-                        />
-                    </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
 
-                    {/* Botón de login */}
-                    <div>
-                        <button
-                            type="submit"
-                            className="w-full py-2 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:ring-2 focus:ring-blue-500"
-                        >
-                            Login
-                        </button>
-                    </div>
-                </form>
+    // Enviar los datos al backend
+    // const response = await fetch("http://localhost:3000/api/login", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ email, password }),
+    // });
 
-                {/* Link para recuperar la contraseña */}
-                <div className="text-center">
-                    <a href="#" className="text-sm text-blue-500 hover:underline">
-                        Forgot password?
-                    </a>
-                </div>
-            </div>
+    // const data = await response.json();
+    // console.log("Datos enviados:", data);
+  };
+
+  // await new Promise((resolve) => setTimeout(resolve, 3000)); // Simula un retraso
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white shadow-lg rounded-lg">
+        <Image
+          className="mx-auto"
+          src="/images/logo_nombre.svg"
+          alt="logo"
+          width={150}
+          height={58}
+        />
+
+        <form onSubmit={handleSubmit} className="space-y-6">
+          {/* Campo de correo electrónico */}
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+             onChange={(e) => setEmail(e.target.value)}
+              required
+              className="mt-2 block w-full px-4 py-2 border border-gray-300 text-black rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Introdueix el teu correu electrònic"
+            />
+          </div>
+
+          {/* Campo de contraseña */}
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-600"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="mt-2 block w-full px-4 py-2 border border-gray-300  text-black rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              placeholder="Introduïu la vostra contrasenya"
+            />
+          </div>
+
+          {/* Botón de login */}
+          <div>
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-[#3e3e3e] hover:shadow-lg transition-all duration-400"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+
+        {/* Link para recuperar la contraseña */}
+        <div className="text-center">
+          <a href="#" className="text-sm text-blue-500 hover:underline">
+            Forgot password?
+          </a>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
---
+++ ilinker/src/app/utils/login/useLogin.js
@@ -0,0 +1,23 @@
+// import { useState } from "react";
+
+// export const useLogin =  async() => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.log("Hola");
+    
+//     // console.log("email", email);
+//     // console.log("password", password);
+//     // Aquí puedes agregar más lógica, como enviar datos al servidor
+//   };
+
+//   return {
+//     email,
+//     setEmail,
+//     password,
+//     setPassword,
+//     handleSubmit,
+//   };
+// };
