45464ca21264a70363f30c1233cf1ed764576f3f
Deraybcbc <a22kevburcac@inspedralbes.cat>
2025-01-30 13:24:05 +0100

    Creacion y modificacion de services, creacion de json para hacer seeders y tambien tests

---
+++ back/back_iLinkerLaravel/app/Services/StudentEducationService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+class StudentEducationService
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function createStudentEducation($data, $studentEducation)
+    {
+
+
+    }
+}
