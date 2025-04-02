9e7c225a64f4cd4b6213a47762240f067e64052c
Purvish Patel <a23purpatmah@inspedralbes.cat>
2025-04-02 09:12:20 +0200

    cambios de el perfil

--- ilinker/src/app/profile/institution/[slug]/page.js
+++ ilinker/src/app/profile/institution/[slug]/page.js
@@ -10,38 +10,38 @@ import InstitutionClient from './InstitueClient';
 import InstituteDoesntExist from './InstituteDoesntExist';
 
 
-// export default async function CompanyProfilePage() {
-//         // const response =  await apiRequest('page/register');
+export default async function CompanyProfilePage() {
+        // const response =  await apiRequest('page/register');
 
-//     // Cambia el return según la vista que quieras mostrar
-//     return <InstitutionClientMe />;
+    // Cambia el return según la vista que quieras mostrar
+    return <InstitutionClientMe />;
 
-//     // return <InstitutionClientNotMe />;
-//     // return <InstituteDoesntExist />;
+    // return <InstitutionClientNotMe />;
+    // return <InstituteDoesntExist />;
+}
+
+
+
+// async function getInstitution(slug) {
+//     try {
+//         const response = await apiRequest(
+//             'institution/' + slug,
+//             'GET'
+//         );
+//         return response;
+//     } catch (error) {
+//         console.error('Error fetching institution:', error);
+//         return null;
+//     }
 // }
 
-
-
-async function getInstitution(slug) {
-    try {
-        const response = await apiRequest(
-            'institution/' + slug,
-            'GET'
-        );
-        return response;
-    } catch (error) {
-        console.error('Error fetching institution:', error);
-        return null;
-    }
-}
-
-export default async function InstitutionPage({ params }) {
-    const { slug } = params;
-    const institution = await getInstitution(slug);
+// export default async function InstitutionPage({ params }) {
+//     const { slug } = params;
+//     const institution = await getInstitution(slug);
     
-    if (!institution) {
-        return <InstituteDoesntExist />;
-    }
+//     if (!institution) {
+//         return <InstituteDoesntExist />;
+//     }
 
-    return <InstitutionClient slug={slug} institution={institution} />;
-}
+//     return <InstitutionClient slug={slug} institution={institution} />;
+// }
