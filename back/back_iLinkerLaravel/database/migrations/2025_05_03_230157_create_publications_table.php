c7ea4ead0bd7b98dc1f5361a52a7f69561ff9f7a
Purvish Patel <a23purpatmah@inspedralbes.cat>
2025-05-04 19:41:51 +0200

    declarar el foreign key en migration

--- back/back_iLinkerLaravel/database/migrations/2025_05_03_230157_create_publications_table.php
+++ back/back_iLinkerLaravel/database/migrations/2025_05_03_230157_create_publications_table.php
@@ -15,8 +15,6 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-
-            $table->morphs('author'); 
             $table->text('content')->nullable(); 
             $table->string('location')->nullable(); 
             $table->boolean('has_media')->default(false); 
--- back/back_iLinkerLaravel/database/migrations/2025_05_03_230616_create_publication_likes_table.php
+++ back/back_iLinkerLaravel/database/migrations/2025_05_03_230616_create_publication_likes_table.php
@@ -15,10 +15,9 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('publication_id');
             $table->unsignedBigInteger('user_id');
-            $table->timestamps();
-
-            $table->foreign('publication_id')->references('id')->on('publications')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('publication_id')->references('id')->on('publications')->onDelete('cascade');
+            $table->timestamps();
 
             // el usuario solo puede dar el link una vez
             $table->unique(['publication_id', 'user_id']);
