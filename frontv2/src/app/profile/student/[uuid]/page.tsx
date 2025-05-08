b49ac2f72f0feef09a7041d8d582e19c2053226f
a22jhepincre <114077543+a22jhepincre@users.noreply.github.com>
2025-05-09 00:23:12 +0200

    try 8

--- frontv2/src/app/profile/student/[uuid]/page.tsx
+++ frontv2/src/app/profile/student/[uuid]/page.tsx
@@ -4,15 +4,31 @@ import {apiRequest} from "@/services/requests/apiRequest";
 import StudentDoesntExist from "@/app/profile/student/[uuid]/StudentDoesntExist";
 import StudentClient from '@/app/profile/student/[uuid]/StudentClient';
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { LoaderContext } from "@/contexts/LoaderContext";
 
 export default function StudentPage() {
     // const uuid = (await params).uuid
     const { uuid } = useParams<{ uuid: string }>();
+    const { showLoader, hideLoader } = useContext(LoaderContext);
+    const [response, setResponse] = useState<any>(null);
 
-    const response = await apiRequest('student/'+uuid);
-    console.log(response)
+    useEffect(()=>{
+        showLoader();
+        apiRequest('student/'+uuid)
+            .then((response) => {
+                setResponse(response)
+            })
+            .catch((e) => {
+                console.error(e);
+            })
+            .finally(() => {
+                hideLoader();
+            });
+    },[]);
 
-    if(response.status === 'success'){
+    if(response){
         return <StudentClient uuid={uuid} student={response.student} experience_group={response.experience_grouped}/>
     }else{
         return <StudentDoesntExist />
