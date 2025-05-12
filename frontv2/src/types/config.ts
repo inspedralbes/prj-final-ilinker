7f63a3c02be19d716bdd80779759236e8afecb45
a22jhepincre <a22jhepincre@inspedralbes.cat>
2025-05-12 12:53:00 +0200

    try 125

--- frontv2/src/types/config.ts
+++ frontv2/src/types/config.ts
@@ -1,18 +1,18 @@
 // src/types/config.ts
 export interface AppConfig {
-  apiUrl: string;
-  appUrl: string;
-  storageUrl: string;
-  socketUrl: string;
-  mapboxToken: string;
+  apiUrl: string | undefined;
+  appUrl: string | undefined;
+  storageUrl: string | undefined;
+  socketUrl: string | undefined;
+  mapboxToken: string | undefined;
 }
 
 const config: AppConfig = {
-  apiUrl:      process.env.NEXT_PUBLIC_API_URL      || 'http://127.0.0.1:8000/api/',
-  appUrl:      process.env.NEXT_PUBLIC_APP_URL      || 'http://localhost:3000',
-  storageUrl:  process.env.NEXT_PUBLIC_STORAGE_URL  || 'http://127.0.0.1:8000/storage/',
-  socketUrl:   process.env.NEXT_PUBLIC_SOCKET_URL   || 'http://127.0.0.1:3777',
-  mapboxToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN || '',
+  apiUrl:      process.env.NEXT_PUBLIC_API_URL      ,
+  appUrl:      process.env.NEXT_PUBLIC_APP_URL      ,
+  storageUrl:  process.env.NEXT_PUBLIC_STORAGE_URL  ,
+  socketUrl:   process.env.NEXT_PUBLIC_SOCKET_URL   ,
+  mapboxToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN ,
 };
 
 export default config;
